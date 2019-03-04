a024a5e04e78a391e38cf38b50aa02445100675f
Caleb Kahan <ckahan00@stuy.edu>
2019-03-04 05:33:05 +0000

    okay got it working mostly

--- MazeDriver.java
+++ MazeDriver.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class MazeDriver{
   public static void main(String[]args){
-      String filename = "Data2.dat";
+      String filename = "Data3.dat";
       try{
         Maze f;
         f = new Maze(filename);//true animates the maze.
